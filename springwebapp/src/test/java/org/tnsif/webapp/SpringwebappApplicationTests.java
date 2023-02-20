4cf537edf3ee256803b87da7c26fa95b458be751
LOHITH SB <LOHITH SB@LAPTOP-HVRVGTB7>
2023-02-20 14:43:14 +0530

    first commit

---
+++ springwebapp/src/test/java/org/tnsif/webapp/SpringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tnsif.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
